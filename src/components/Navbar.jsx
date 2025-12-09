35deaea6ee39d1ec9550f34e440edc1421514a92
Saon <saonbiswas75@outlook.com>
2025-12-09 22:12:22 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,8 @@ import React, { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { ShoppingCart } from "lucide-react"; // --- 1. Import Icon ---
 import Logo from "../assets/logo.png";
-import { AuthContext } from "../context/authcontext";
-import { CartContext } from "../context/cartcontext"; // --- 2. Import CartContext ---
+import { AuthContext } from "../context/AuthContext";
+import { CartContext } from "../context/CartContext"; // --- 2. Import CartContext ---
 import { supabase } from "../supabase"; 
 
 const Navbar = () => {
