69a3edd4ba74fb255dabf07ab544dda924f41754
Saon <saonbiswas75@outlook.com>
2025-12-09 21:58:17 +0530

    *facepalms*

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { AuthProvider } from "./context/authcontext.jsx";
-import CartProvider from "./context/cartcontext.jsx";
+import { AuthProvider } from "./context/AuthContext.jsx";
+import CartProvider from "./context/CartContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
